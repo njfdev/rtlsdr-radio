c7a9c0d469a3faf71994b05c2f262b7e5972a690
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-16 16:11:23 -0400

    Build Intel and Arm on Native Machine Rather than Cross-Compiling

--- build_scripts/nrsc5.sh
+++ build_scripts/nrsc5.sh
@@ -7,18 +7,8 @@ cd nrsc5
 git reset --hard a57dd5b5f93e08d9ccdeb5f6b670a16d7566f8f1
 mkdir build
 cd build
-if [ "$TAURI_PLATFORM" = "macos" ]; then
-  if [ "$TAURI_ARCH" = "aarch64" ]; then
-    cmake -DCMAKE_OSX_ARCHITECTURES=arm64 -DUSE_FAAD2=OFF ../
-  else
-    cmake -DCMAKE_OSX_ARCHITECTURES=$TAURI_ARCH -DUSE_FAAD2=OFF ../
-  fi
-    BINARY_BUILD_TARGET_NAME="$TAURI_ARCH-apple-darwin"
-else
-  cmake -DUSE_FAAD2=OFF ../
-  BINARY_BUILD_TARGET_NAME=$(rustc -vV | sed -n 's|host: ||p')
-fi
+cmake -DUSE_FAAD2=OFF ../
 make
 cd ../../
 mkdir bin
-mv ./nrsc5/build/src/nrsc5 ./bin/nrsc5-$BINARY_BUILD_TARGET_NAME
+mv ./nrsc5/build/src/nrsc5 ./bin/nrsc5-$(rustc -vV | sed -n 's|host: ||p')
