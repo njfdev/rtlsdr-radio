61b92e43f224cb2d4c21347d40ad5827947aca16
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-16 16:50:04 -0400

    Fix Building Error on Windows

--- build_scripts/run_all.py
+++ build_scripts/run_all.py
@@ -2,6 +2,6 @@ import subprocess
 import sys
 
 if "win32" in sys.platform:
-    subprocess.run([".\\build_scripts\\nrsc5.ps1"], check=True)
+    subprocess.run(["pwsh", ".\\build_scripts\\nrsc5.ps1"], check=True)
 else:
     subprocess.run(["sh", "./build_scripts/nrsc5.sh"], check=True)
