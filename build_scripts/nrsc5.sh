086918914af9163c92a3e03d621d1da7412e41cb
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-16 14:28:30 -0400

    Update Build Script to Build for Either Mac Arch

--- build_scripts/nrsc5.sh
+++ build_scripts/nrsc5.sh
@@ -5,8 +5,18 @@ cd nrsc5
 git reset --hard a57dd5b5f93e08d9ccdeb5f6b670a16d7566f8f1
 mkdir build
 cd build
-cmake ../
+if [ "$TAURI_PLATFORM" = "macos" ]; then
+  if [ "$TAURI_ARCH" = "aarch64" ]; then
+    cmake -DCMAKE_OSX_ARCHITECTURES=arm64 ../
+  else
+    cmake -DCMAKE_OSX_ARCHITECTURES=$TAURI_ARCH ../
+  fi
+    BINARY_BUILD_TARGET_NAME="$TAURI_ARCH-apple-darwin"
+else
+  cmake ../
+  BINARY_BUILD_TARGET_NAME=$(rustc -vV | sed -n 's|host: ||p')
+fi
 make
 cd ../../
 mkdir bin
-mv ./nrsc5/build/src/nrsc5 ./bin/nrsc5-$(rustc -vV | sed -n 's|host: ||p')
+mv ./nrsc5/build/src/nrsc5 ./bin/nrsc5-$BINARY_BUILD_TARGET_NAME
