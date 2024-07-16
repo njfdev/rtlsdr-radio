927d5ddc093361050d73abede5b9e1b92790d421
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-16 17:08:03 -0400

    Use MSYS2 for Building on Windows

--- build_scripts/run_all.py
+++ build_scripts/run_all.py
@@ -2,6 +2,6 @@ import subprocess
 import sys
 
 if "win32" in sys.platform:
-    subprocess.run(["pwsh", ".\\build_scripts\\nrsc5.ps1"], check=True)
+    subprocess.run(["C:/msys64/usr/bin/bash.exe", "-c", "./build_scripts/nrsc5.sh"], check=True)
 else:
     subprocess.run(["sh", "./build_scripts/nrsc5.sh"], check=True)
