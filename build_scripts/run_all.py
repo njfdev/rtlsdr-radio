d8b90091ad9311514a3f7ffab372401ba2d8a8cc
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-17 11:13:50 -0400

    Move to Working Directory When Running Windows Build Script

--- build_scripts/run_all.py
+++ build_scripts/run_all.py
@@ -1,7 +1,10 @@
 import subprocess
 import sys
+import os
 
 if "win32" in sys.platform:
-    subprocess.run(["C:/msys64/usr/bin/bash.exe", "-l", "-c", "./build_scripts/nrsc5.sh"], check=True)
+    cwd = os.getcwd()
+
+    subprocess.run(["C:/msys64/usr/bin/bash.exe", "-l", "-c", "(cd " + cwd + " && ./build_scripts/nrsc5.sh)"], check=True)
 else:
     subprocess.run(["sh", "./build_scripts/nrsc5.sh"], check=True)
