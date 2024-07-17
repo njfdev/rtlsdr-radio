e26217dabf6ffd0819f98dd6a48105afb293e0e7
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-17 11:59:21 -0400

    Install Windows Dependencies with Pacman in Build Script

--- build_scripts/nrsc5.sh
+++ build_scripts/nrsc5.sh
@@ -1,5 +1,10 @@
 # Note: FAAD2 is turned off because it causes issues in GitHub Actions and is not needed
 
+# if pacman exists, install the dependencies
+if [ -x "$(command -v pacman)" ]; then
+pacman -S --noconfirm autoconf automake git gzip make mingw-w64-i686-gcc mingw-w64-i686-cmake mingw-w64-i686-libtool patch tar xz
+fi
+
 mkdir build
 cd build
 git clone https://github.com/theori-io/nrsc5.git
@@ -7,7 +12,7 @@ cd nrsc5
 git reset --hard a57dd5b5f93e08d9ccdeb5f6b670a16d7566f8f1
 mkdir build
 cd build
-cmake -DUSE_FAAD2=OFF ../
+cmake ../
 make
 cd ../../
 mkdir bin
