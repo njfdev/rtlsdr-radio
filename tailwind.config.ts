66c0a7d2f63bcdac40cc9449e77b268b1f3c0a77
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-17 10:42:14 -0400

    Fix Status Dots not Showing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,18 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  safelist: [
+    "before:bg-red-500",
+    "before:bg-yellow-500",
+    "before:bg-green-500",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -75,6 +80,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
