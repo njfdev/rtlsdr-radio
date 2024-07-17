367cc887031e3d65a46eaa84011ec0daa3e5740a
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-17 10:58:33 -0400

    Add -l Flag to Windows Build Script to Pass Env Variables

--- build_scripts/run_all.py
+++ build_scripts/run_all.py
@@ -2,6 +2,6 @@ import subprocess
 import sys
 
 if "win32" in sys.platform:
-    subprocess.run(["C:/msys64/usr/bin/bash.exe", "-c", "./build_scripts/nrsc5.sh"], check=True)
+    subprocess.run(["C:/msys64/usr/bin/bash.exe", "-l", "-c", "./build_scripts/nrsc5.sh"], check=True)
 else:
     subprocess.run(["sh", "./build_scripts/nrsc5.sh"], check=True)
