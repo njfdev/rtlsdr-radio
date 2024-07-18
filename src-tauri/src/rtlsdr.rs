455639d15153b7e8772d1c5fefc83744d570f1c2
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-18 09:48:15 -0400

    refactor: Move rtlsdr_device Into Arc for Easier Access

--- src-tauri/src/rtlsdr.rs
+++ src-tauri/src/rtlsdr.rs
@@ -7,7 +7,7 @@ pub mod rtlsdr {
 
   pub struct RtlSdrState(Arc<Mutex<RtlSdrData>>);
   pub struct RtlSdrData {
-    pub rtlsdr_device: Option<Device>,
+    pub rtlsdr_device: Option<Arc<Device>>,
     pub radio_stream_thread: Option<thread::JoinHandle<()>>,
     pub shutdown_flag: Arc<AtomicBool>
   }
@@ -20,7 +20,7 @@ pub mod rtlsdr {
     pub fn connect_to_sdr(&self) {
       let args = "driver=rtlsdr";
       let device = Device::new(args).expect("Failed to create a SoapySDR device");
-      self.0.clone().lock().unwrap().rtlsdr_device = Some(device);
+      self.0.clone().lock().unwrap().rtlsdr_device = Some(Arc::new(device));
     }
 
     pub fn start_stream(&self, window: Window, fm_freq: String) {
@@ -30,17 +30,19 @@ pub mod rtlsdr {
         self.connect_to_sdr();
       }
 
+      let rtlsdr_dev = rtlsdr_state.lock().unwrap().rtlsdr_device.clone().unwrap();
+
       // set sample rate
       let sample_rate = 2.048e6;
-      rtlsdr_state.lock().unwrap().rtlsdr_device.as_ref().unwrap().set_sample_rate(Rx, 0, sample_rate);
+      rtlsdr_dev.set_sample_rate(Rx, 0, sample_rate);
 
       // set center frequency
       let sdr_freq = fm_freq.parse::<f64>().expect("FM Frequency could not be parsed as a float") * 1_000_000.0;
-      rtlsdr_state.lock().unwrap().rtlsdr_device.as_ref().unwrap().set_frequency(Rx, 0, sdr_freq, "").expect("Failed to set frequency");
+      rtlsdr_dev.set_frequency(Rx, 0, sdr_freq, "").expect("Failed to set frequency");
 
 
       // create an RX stream
-      let mut rx_stream = rtlsdr_state.lock().unwrap().rtlsdr_device.as_ref().unwrap().rx_stream::<Complex32>(&[0]).expect("Failed to create RX stream");
+      let mut rx_stream = rtlsdr_dev.rx_stream::<Complex32>(&[0]).expect("Failed to create RX stream");
 
       // activate the stream
       rx_stream.activate(None).expect("Failed to activate RX stream");
