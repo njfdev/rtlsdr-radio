516b4f976babd78ef76620fa88d049aad948c208
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-19 20:33:05 -0400

    Add Tab Menu to Switch Between HD Radio and FM Radio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
 import Nrsc5Controls from "@/components/Nrsc5Controls";
 import SoapySdrControls from "@/components/SoapySdrConstrols";
 import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Home() {
   return (
     <main className="flex h-screen w-screen flex-col items-center align-middle justify-center p-24 gap-4">
-      <Nrsc5Controls />
-      <Separator className="w-[44rem]" />
-      <SoapySdrControls />
+      <Tabs
+        defaultValue="hd-radio"
+        className="flex flex-col justify-start items-center align-middle h-[18rem]"
+      >
+        <TabsList>
+          <TabsTrigger value="hd-radio">HD Radio</TabsTrigger>
+          <TabsTrigger value="fm-radio">FM Radio</TabsTrigger>
+        </TabsList>
+        <TabsContent value="hd-radio">
+          <Nrsc5Controls />
+        </TabsContent>
+        <TabsContent value="fm-radio">
+          <SoapySdrControls />
+        </TabsContent>
+      </Tabs>
     </main>
   );
 }
