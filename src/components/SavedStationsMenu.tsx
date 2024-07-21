036d811d926648d3e4cf277af7b234434cdd7c97
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-21 15:10:13 -0400

    refactor: Move SavedStationCard Into Separate Function

--- src/components/SavedStationsMenu.tsx
+++ src/components/SavedStationsMenu.tsx
@@ -61,90 +61,102 @@ export default function SavedStationsMenu({
             <CardTitle>Saved Stations</CardTitle>
           </CardHeader>
           <CardContent className="grid gap-2">
-            {stations &&
-              stations.map((station) => {
-                const isCurrentStationPlaying =
-                  isStationPlaying &&
-                  currentStation &&
-                  areStationsEqual(station, currentStation);
-                const isLoading = areStationsEqual(loadingStation, station);
-
-                return (
-                  <Card
-                    key={`${station.type}-${station.frequency}-${
-                      station.channel || 0
-                    }`}
-                  >
-                    <CardHeader>
-                      <div className="flex justify-between align-middle">
-                        <CardTitle className="text-lg">
-                          {station.title}
-                        </CardTitle>
-                        <Star
-                          size="22px"
-                          className={`transition-all ${
-                            station.isFavorite
-                              ? "fill-yellow-300 stroke-yellow-300"
-                              : "fill-black"
-                          }`}
-                          onClick={() => {
-                            updateStation(station, {
-                              ...station,
-                              isFavorite: !station.isFavorite,
-                            });
-                          }}
-                        />
-                      </div>
-                    </CardHeader>
-                    <CardContent>
-                      <p>
-                        Type:{" "}
-                        {station.type == StationType.HDRadio
-                          ? "HD Radio"
-                          : station.type == StationType.FMRadio
-                          ? "FM Radio"
-                          : "Unknown"}
-                      </p>
-                      <p>Frequency: {station.frequency}</p>
-                      {station.channel && <p>Channel: {station.channel!}</p>}
-                    </CardContent>
-                    <CardFooter>
-                      <Button
-                        onClick={() => {
-                          if (isCurrentStationPlaying) {
-                            updateRequestedStation(undefined);
-                          } else {
-                            updateRequestedStation({
-                              type: station.type,
-                              frequency: station.frequency,
-                              channel: station.channel,
-                            });
-                          }
-                        }}
-                        disabled={isLoading}
-                        variant={
-                          isCurrentStationPlaying ? "secondary" : "default"
-                        }
-                      >
-                        {isLoading ? (
-                          <>
-                            <Loader2 className="animate-spin mr-2" /> Loading...
-                          </>
-                        ) : (
-                          <>
-                            {isCurrentStationPlaying
-                              ? "Stop Station"
-                              : "Start Station"}
-                          </>
-                        )}
-                      </Button>
-                    </CardFooter>
-                  </Card>
-                );
-              })}
+            {stations?.map((station) => (
+              <SavedStationCard
+                key={`${station.type}-${station.frequency}-${
+                  station.channel || 0
+                }`}
+                station={station}
+                isStationPlaying={isStationPlaying}
+                currentStation={currentStation}
+                loadingStation={loadingStation}
+                updateRequestedStation={updateRequestedStation}
+              />
+            ))}
           </CardContent>
         </Card>
       </div>
     </>
+  );
+}
+
+function SavedStationCard({
+  station,
+  isStationPlaying,
+  currentStation,
+  loadingStation,
+  updateRequestedStation,
+}: {
+  station: StationDetails;
+  currentStation: Station | undefined;
+  isStationPlaying: boolean;
+  loadingStation: Station | undefined;
+  updateRequestedStation: (station?: Station) => void;
+}) {
+  const isCurrentStationPlaying =
+    isStationPlaying &&
+    currentStation &&
+    areStationsEqual(station, currentStation);
+  const isLoading = areStationsEqual(loadingStation, station);
+
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex justify-between align-middle">
+          <CardTitle className="text-lg">{station.title}</CardTitle>
+          <Star
+            size="22px"
+            className={`transition-all ${
+              station.isFavorite
+                ? "fill-yellow-300 stroke-yellow-300"
+                : "fill-black"
+            }`}
+            onClick={() => {
+              updateStation(station, {
+                ...station,
+                isFavorite: !station.isFavorite,
+              });
+            }}
+          />
+        </div>
+      </CardHeader>
+      <CardContent>
+        <p>
+          Type:{" "}
+          {station.type == StationType.HDRadio
+            ? "HD Radio"
+            : station.type == StationType.FMRadio
+            ? "FM Radio"
+            : "Unknown"}
+        </p>
+        <p>Frequency: {station.frequency}</p>
+        {station.channel && <p>Channel: {station.channel!}</p>}
+      </CardContent>
+      <CardFooter>
+        <Button
+          onClick={() => {
+            if (isCurrentStationPlaying) {
+              updateRequestedStation(undefined);
+            } else {
+              updateRequestedStation({
+                type: station.type,
+                frequency: station.frequency,
+                channel: station.channel,
+              });
+            }
+          }}
+          disabled={isLoading}
+          variant={isCurrentStationPlaying ? "secondary" : "default"}
+        >
+          {isLoading ? (
+            <>
+              <Loader2 className="animate-spin mr-2" /> Loading...
+            </>
+          ) : (
+            <>{isCurrentStationPlaying ? "Stop Station" : "Start Station"}</>
+          )}
+        </Button>
+      </CardFooter>
+    </Card>
   );
 }
