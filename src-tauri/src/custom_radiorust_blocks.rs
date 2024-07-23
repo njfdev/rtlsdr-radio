d82168785fdc9d2626110623ec51a6f27180e981
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-23 17:29:41 -0400

    Add Logic For Demodulating FM Signal

--- src-tauri/src/custom_radiorust_blocks.rs
+++ src-tauri/src/custom_radiorust_blocks.rs
@@ -1,9 +1,10 @@
 pub mod custom_radiorust_blocks {
+    use biquad::{coefficients, Biquad, Coefficients, DirectForm1, ToHertz};
     use radiorust::{
         flow::{new_receiver, new_sender, Message, ReceiverConnector, SenderConnector},
         impl_block_trait,
         numbers::Float,
-        prelude::Complex,
+        prelude::{ChunkBufPool, Complex},
         signal::Signal,
     };
     use tokio::spawn;
@@ -18,11 +19,17 @@ pub mod custom_radiorust_blocks {
 
     impl<Flt> AmDemod<Flt>
     where
-        Flt: Float,
+        Flt: Float + Into<f64>,
     {
         pub fn new() -> Self {
             let (mut receiver, receiver_connector) = new_receiver::<Signal<Complex<Flt>>>();
             let (sender, sender_connector) = new_sender::<Signal<Complex<Flt>>>();
+
+            let mut buf_pool = ChunkBufPool::<Complex<Flt>>::new();
+
+            let mut coeffs: Option<Coefficients<f64>> = None;
+            let mut filter: Option<DirectForm1<f64>> = None;
+
             spawn(async move {
                 loop {
                     let Ok(signal) = receiver.recv().await else {
@@ -33,11 +40,41 @@ pub mod custom_radiorust_blocks {
                             sample_rate,
                             chunk: input_chunk,
                         } => {
-                            // pass through signal
+                            // if a lowpass filter isn't already create, create one with sample_rate info
+                            if coeffs.is_none() {
+                                // create a lowpass filter with biquad
+                                // no AM broadcast should have frequencies higher than 15khz
+                                let cutoff_hz = 15_000.0;
+                                coeffs = Some(
+                                    Coefficients::<f64>::from_params(
+                                        biquad::Type::LowPass,
+                                        (sample_rate as f64).hz(),
+                                        (cutoff_hz as f64).hz(),
+                                        0.707,
+                                    )
+                                    .unwrap(),
+                                );
+                                filter = Some(DirectForm1::<f64>::new(coeffs.unwrap()));
+                            }
+
+                            let mut output_chunk = buf_pool.get_with_capacity(input_chunk.len());
+
+                            // get the magnitude for each sample
+                            for &sample in input_chunk.iter() {
+                                let magnitude = AmDemod::calc_magnitude(sample);
+
+                                // apply the lowpass filter (easy way to demodulate am)
+                                let filtered_magnitude = filter.unwrap().run(magnitude.into());
+                                output_chunk.push(Complex {
+                                    re: Flt::from(filtered_magnitude).unwrap(),
+                                    im: Flt::from(0.0).unwrap(),
+                                });
+                            }
+
                             let Ok(()) = sender
                                 .send(Signal::Samples {
                                     sample_rate,
-                                    chunk: input_chunk,
+                                    chunk: output_chunk.finalize(),
                                 })
                                 .await
                             else {
@@ -56,6 +93,10 @@ pub mod custom_radiorust_blocks {
                 receiver_connector,
                 sender_connector,
             }
+        }
+
+        fn calc_magnitude(c: Complex<Flt>) -> f64 {
+            (c.re.powi(2) + c.im.powi(2)).sqrt().into()
         }
     }
 }
