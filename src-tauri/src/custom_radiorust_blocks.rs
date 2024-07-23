4546cdb51a820874c6d49c87a44cec6cdc03ae18
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-23 17:01:30 -0400

    Add Structure Needed For AmDemod Block

--- src-tauri/src/custom_radiorust_blocks.rs
+++ src-tauri/src/custom_radiorust_blocks.rs
@@ -2,34 +2,54 @@ pub mod custom_radiorust_blocks {
     use radiorust::{
         flow::{new_receiver, new_sender, Message, ReceiverConnector, SenderConnector},
         impl_block_trait,
+        numbers::Float,
+        prelude::Complex,
+        signal::Signal,
     };
     use tokio::spawn;
 
-    pub struct AmDemod<T> {
-        receiver_connector: ReceiverConnector<T>,
-        sender_connector: SenderConnector<T>,
+    pub struct AmDemod<Flt> {
+        receiver_connector: ReceiverConnector<Signal<Complex<Flt>>>,
+        sender_connector: SenderConnector<Signal<Complex<Flt>>>,
     }
 
-    impl_block_trait! { <T> Consumer<T> for AmDemod<T> }
-    impl_block_trait! { <T> Producer<T> for AmDemod<T> }
+    impl_block_trait! { <Flt> Consumer<Signal<Complex<Flt>>> for AmDemod<Flt> }
+    impl_block_trait! { <Flt> Producer<Signal<Complex<Flt>>> for AmDemod<Flt> }
 
-    // this is just a NOP block from the docs for now
-    impl<T> AmDemod<T>
+    impl<Flt> AmDemod<Flt>
     where
-        T: Message + Send + 'static,
+        Flt: Float,
     {
-        /// Creates a block which does nothing but pass data through
         pub fn new() -> Self {
-            let (mut receiver, receiver_connector) = new_receiver::<T>();
-            let (sender, sender_connector) = new_sender::<T>();
+            let (mut receiver, receiver_connector) = new_receiver::<Signal<Complex<Flt>>>();
+            let (sender, sender_connector) = new_sender::<Signal<Complex<Flt>>>();
             spawn(async move {
                 loop {
-                    let Ok(msg) = receiver.recv().await else {
+                    let Ok(signal) = receiver.recv().await else {
                         return;
                     };
-                    let Ok(()) = sender.send(msg).await else {
-                        return;
-                    };
+                    match signal {
+                        Signal::Samples {
+                            sample_rate,
+                            chunk: input_chunk,
+                        } => {
+                            // pass through signal
+                            let Ok(()) = sender
+                                .send(Signal::Samples {
+                                    sample_rate,
+                                    chunk: input_chunk,
+                                })
+                                .await
+                            else {
+                                return;
+                            };
+                        }
+                        Signal::Event(event) => {
+                            let Ok(()) = sender.send(Signal::Event(event)).await else {
+                                return;
+                            };
+                        }
+                    }
                 }
             });
             Self {
