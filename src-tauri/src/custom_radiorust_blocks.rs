7ed803faf77114c6c4c69ed2df762449b38a8eca
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-25 13:09:08 -0400

    Add Logic for Demodulating RBDS Signal

--- src-tauri/src/custom_radiorust_blocks.rs
+++ src-tauri/src/custom_radiorust_blocks.rs
@@ -1,7 +1,17 @@
 pub mod custom_radiorust_blocks {
-    use std::sync::{Arc, Mutex};
+    use std::{
+        f64::consts::PI,
+        fmt::Debug,
+        fs,
+        io::BufWriter,
+        sync::{Arc, Mutex},
+    };
 
-    use biquad::{Biquad, Coefficients, DirectForm1, ToHertz, Type, Q_BUTTERWORTH_F64};
+    use biquad::{
+        Biquad, Coefficients, DirectForm1, DirectForm2Transposed, ToHertz, Type, Q_BUTTERWORTH_F32,
+        Q_BUTTERWORTH_F64,
+    };
+    use hound::{WavSpec, WavWriter};
     use radiorust::{
         blocks,
         flow::{new_receiver, new_sender, Consumer, Message, ReceiverConnector, SenderConnector},
@@ -104,6 +114,10 @@ pub mod custom_radiorust_blocks {
         }
     }
 
+    // constants for RDBS Decoding
+    const RDBS_CARRIER_FREQ: f64 = 57_000.0;
+    const RDBS_CLOCK_FREQ: f64 = RDBS_CARRIER_FREQ / 48.0; // as defined in the RDS spec
+
     pub struct RbdsDecode<Flt> {
         receiver_connector: ReceiverConnector<Signal<Complex<Flt>>>,
         window: Window,
@@ -118,7 +132,13 @@ pub mod custom_radiorust_blocks {
         pub fn new(window: Window) -> Self {
             let (mut receiver, receiver_connector) = new_receiver::<Signal<Complex<Flt>>>();
 
-            let mut chunk_num = 0;
+            // setup Wav file writer
+            let mut wav_writer: Arc<Mutex<Option<WavWriter<BufWriter<fs::File>>>>> =
+                Arc::new(Mutex::new(None));
+
+            let mut bandpass_filter: Option<DirectForm2Transposed<f64>> = None;
+
+            let mut lowpass_filter: Option<DirectForm1<f64>> = None;
 
             spawn(async move {
                 loop {
@@ -130,6 +150,70 @@ pub mod custom_radiorust_blocks {
                             sample_rate,
                             chunk: input_chunk,
                         } => {
+                            // Step 1: Band-Pass Filter to "select" RBDS sub-carrier
+                            if bandpass_filter.is_none() {
+                                let filter_coefficients = Coefficients::<f64>::from_params(
+                                    Type::BandPass,
+                                    ToHertz::hz(sample_rate),
+                                    ToHertz::hz(RDBS_CARRIER_FREQ),
+                                    Q_BUTTERWORTH_F64,
+                                );
+                                bandpass_filter = Some(DirectForm2Transposed::<f64>::new(
+                                    filter_coefficients.unwrap(),
+                                ));
+                            }
+                            // NOTE: The input_chunk should already be FM demodulated, so the imaginary part of the Complex Number is 0 and can be ignored
+                            let bandpassed_samples: Vec<f64> = input_chunk
+                                .iter()
+                                .map(|&sample| bandpass_filter.unwrap().run(sample.re.into()))
+                                .collect();
+
+                            // Step 2: Downmix to baseband
+                            let downmixed_samples: Vec<f64> = bandpassed_samples
+                                .iter()
+                                .enumerate()
+                                .map(|(i, &sample)| {
+                                    let t = (i as f64 / sample_rate);
+                                    sample * (2.0 * PI * RDBS_CARRIER_FREQ * t).cos()
+                                })
+                                .collect();
+
+                            // Step 3: Apply Lowpass Filter to Remove High-Frequency Component (the 57KHz sub-carrier)
+                            if lowpass_filter.is_none() {
+                                let filter_coefficients = Coefficients::<f64>::from_params(
+                                    Type::LowPass,
+                                    ToHertz::hz(sample_rate),
+                                    ToHertz::hz(RDBS_CARRIER_FREQ),
+                                    Q_BUTTERWORTH_F64,
+                                );
+                                lowpass_filter =
+                                    Some(DirectForm1::<f64>::new(filter_coefficients.unwrap()));
+                            }
+                            let lowpassed_samples: Vec<f64> = downmixed_samples
+                                .iter()
+                                .map(|sample| lowpass_filter.unwrap().run(*sample))
+                                .collect();
+
+                            // Step 4: Save to WAV file for Testing
+                            if wav_writer.clone().lock().unwrap().is_none() {
+                                let wav_spec = WavSpec {
+                                    channels: 1,
+                                    sample_rate: sample_rate as u32,
+                                    bits_per_sample: 32,
+                                    sample_format: hound::SampleFormat::Float,
+                                };
+                                *(wav_writer.lock().unwrap()) =
+                                    Some(WavWriter::create("rbds_output.wav", wav_spec).unwrap());
+                            }
+                            for sample in lowpassed_samples {
+                                wav_writer
+                                    .lock()
+                                    .unwrap()
+                                    .as_mut()
+                                    .unwrap()
+                                    .write_sample(sample as f32)
+                                    .unwrap();
+                            }
 
                             // unlike other blocks, this just "eats" the signal and does not pass it on
                         }
