4e5381068b11144e5c9a0fe6dec07ae703be6f7c
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-08-03 14:12:42 -0400

    Fix Runtime Search Path for Linux and macOS

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,7 +1,10 @@
 fn main() {
     // Tell Cargo the place of the shared libraries
     println!("cargo:rustc-link-search=../build/SoapySDR/build/lib");
-    println!("cargo:rustc-link-search=native=./resources/libs");
+    // for linux
+    println!("cargo:rustc-link-arg=-Wl,-rpath,'$ORIGIN'/resources/libs");
+    // for macos
+    println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path/resources/libs");
 
     tauri_build::build()
 }
