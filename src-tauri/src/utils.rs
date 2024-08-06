ac3542500a994fdcee458efd129bb374e936b14c
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-08-05 20:14:33 -0400

    Fix Loading LibUSB

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,5 +1,5 @@
 pub mod utils {
-    use std::env;
+    use std::{env, fs, path::PathBuf};
 
     use libloading::Library;
     use tauri::{App, Manager};
@@ -21,14 +21,33 @@ pub mod utils {
             ".so"
         };
 
+        // Find the first libusb shared library in the resources/lib directory
+        let libusb_path = find_libusb_library(&resource_dir, os_ext)
+            .expect("Failed to find libusb shared library");
+
         // load libusb shared library
         unsafe {
-            let _lib = Library::new(format!(
-                "{}/resources/lib/libusb-1.0.0{}",
-                resource_dir.as_os_str().to_str().unwrap(),
-                os_ext
-            ))
-            .expect("Failed to load shared library");
+            let _lib = Library::new(libusb_path).expect("Failed to load shared library");
         }
+    }
+
+    fn find_libusb_library(resource_dir: &PathBuf, os_ext: &str) -> Option<PathBuf> {
+        let lib_dir = resource_dir.join("resources/lib");
+        let entries = fs::read_dir(lib_dir).ok()?;
+
+        for entry in entries {
+            let entry = entry.ok()?;
+            let path = entry.path();
+            if path.is_file() {
+                if let Some(file_name) = path.file_name() {
+                    if let Some(file_name_str) = file_name.to_str() {
+                        if file_name_str.starts_with("libusb") && file_name_str.ends_with(os_ext) {
+                            return Some(path);
+                        }
+                    }
+                }
+            }
+        }
+        None
     }
 }
