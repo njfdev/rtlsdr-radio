29062a34925234090df5d6353b154a781fa8b613
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-08-09 12:26:19 -0400

    Add State Types for Mode S and ADS-B

--- src-tauri/src/modes/types.rs
+++ src-tauri/src/modes/types.rs
@@ -5,6 +5,15 @@ pub const MODES_PREAMBLE_US: usize = 8; // preamble length in microseconds
 
 pub const AIS_CHARSET: &str = "?ABCDEFGHIJKLMNOPQRSTUVWXYZ????? ???????????????0123456789??????";
 
+pub struct ModeSState {
+    aircraft: Vec<AircraftState>,
+}
+
+pub struct AircraftState {
+    icao_address: u32,
+    adsb_state: AdsbState,
+}
+
 // ADS-B
 #[derive(PartialEq)]
 pub enum AltitudeSource {
@@ -16,4 +25,15 @@ pub enum AltitudeSource {
 pub enum AirspeedType {
     IAS, // indicated airspeed
     TAS, // true airspeed
+}
+
+pub struct AdsbState {
+    cpr_position: CprPosition,
+}
+
+pub struct CprPosition {
+    cpr_even_lat: u32,
+    cpr_odd_lat: u32,
+    cpr_even_lon: u32,
+    cpr_odd_lon: u32,
 }
