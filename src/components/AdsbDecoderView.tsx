a71c1c71b95d949edb3852578b6e73cebf54d5b0
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-08-11 17:10:26 -0400

    Use Stadia Maps Alidade Satellite

--- src/components/AdsbDecoderView.tsx
+++ src/components/AdsbDecoderView.tsx
@@ -48,27 +48,7 @@ export default function AdsbDecoderView() {
           zoom: 14,
         }}
         style={{ width: 600, height: 400 }}
-        mapStyle={{
-          version: 8,
-          sources: {
-            "raster-tiles": {
-              type: "raster",
-              tiles: ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
-              tileSize: 256,
-              attribution:
-                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            },
-          },
-          layers: [
-            {
-              id: "simple-tiles",
-              type: "raster",
-              source: "raster-tiles",
-              minzoom: 0,
-              maxzoom: 22,
-            },
-          ],
-        }}
+        mapStyle="https://tiles.stadiamaps.com/styles/alidade_satellite.json"
       ></Map>
 
       {modesState && (
