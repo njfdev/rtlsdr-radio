0a22c8eae11a9dbd4ab6a86892cca0c4b37c5509
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-08-18 12:46:35 -0400

    Make Automatic Light/Dark Mode Logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Station } from "@/lib/types";
 import { getCurrentWebviewWindow } from "@tauri-apps/api/webviewWindow";
 import { useEffect, useState } from "react";
+import { ThemeProvider } from "./components/ThemeProvider";
 
 const appWindow = getCurrentWebviewWindow();
 
@@ -47,45 +48,47 @@ export default function App() {
   });
 
   return (
-    <main className="flex flex-col h-screen w-screen gap-4">
-      <Tabs
-        value={currentTab}
-        onValueChange={async (newTab) => {
-          if (isSdrInUse) {
-            if (currentStation) {
-              setRequestedStation(undefined);
-            } else {
-              setShouldStopAdsb(true);
-              setRequestedTab(newTab);
+    <ThemeProvider>
+      <main className="flex flex-col h-screen w-screen gap-4">
+        <Tabs
+          value={currentTab}
+          onValueChange={async (newTab) => {
+            if (isSdrInUse) {
+              if (currentStation) {
+                setRequestedStation(undefined);
+              } else {
+                setShouldStopAdsb(true);
+                setRequestedTab(newTab);
+              }
             }
-          }
-          setCurrentTab(newTab);
-        }}
-        className="flex flex-col justify-start items-center align-middle h-screen w-screen overflow-hidden"
-      >
-        <TabsList className="mt-4 mb-2">
-          <TabsTrigger value="radio">Radio</TabsTrigger>
-          <TabsTrigger value="adsb">ADS-B</TabsTrigger>
-        </TabsList>
-        <TabsContent value="radio" className="grow w-full overflow-hidden">
-          <RadioView
-            isSdrInUse={isSdrInUse}
-            setIsSdrInUse={setIsSdrInUse}
-            currentStation={currentStation}
-            setCurrentStation={setCurrentStation}
-            requestedStation={requestedStation}
-            setRequestedStation={setRequestedStation}
-          />
-        </TabsContent>
-        <TabsContent value="adsb" className="grow w-full overflow-hidden">
-          <AdsbDecoderView
-            //isSdrInUse={isSdrInUse}
-            setIsSdrInUse={setIsSdrInUse}
-            shouldStop={shouldStopAdsb}
-            setShouldStop={setShouldStopAdsb}
-          />
-        </TabsContent>
-      </Tabs>
-    </main>
+            setCurrentTab(newTab);
+          }}
+          className="flex flex-col justify-start items-center align-middle h-screen w-screen overflow-hidden"
+        >
+          <TabsList className="mt-4 mb-2">
+            <TabsTrigger value="radio">Radio</TabsTrigger>
+            <TabsTrigger value="adsb">ADS-B</TabsTrigger>
+          </TabsList>
+          <TabsContent value="radio" className="grow w-full overflow-hidden">
+            <RadioView
+              isSdrInUse={isSdrInUse}
+              setIsSdrInUse={setIsSdrInUse}
+              currentStation={currentStation}
+              setCurrentStation={setCurrentStation}
+              requestedStation={requestedStation}
+              setRequestedStation={setRequestedStation}
+            />
+          </TabsContent>
+          <TabsContent value="adsb" className="grow w-full overflow-hidden">
+            <AdsbDecoderView
+              //isSdrInUse={isSdrInUse}
+              setIsSdrInUse={setIsSdrInUse}
+              shouldStop={shouldStopAdsb}
+              setShouldStop={setShouldStopAdsb}
+            />
+          </TabsContent>
+        </Tabs>
+      </main>
+    </ThemeProvider>
   );
 }
