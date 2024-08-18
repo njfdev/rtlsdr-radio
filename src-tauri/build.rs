a8ee783c1d47447b3d14d9be30de505e413a66be
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-08-18 18:01:18 -0400

    Fix Build Script for Building Debug Version

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -4,9 +4,7 @@ fn main() {
     // for linux
     println!("cargo:rustc-link-arg=-Wl,-rpath,'$ORIGIN'/resources/lib");
     // for macos
-    #[cfg(debug_assertions)]
     println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path/resources/lib");
-    #[cfg(not(debug_assertions))]
     println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path/../Resources/resources/lib");
 
     tauri_build::build()
