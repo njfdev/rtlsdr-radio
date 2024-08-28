df589a43503b8ac850b573c70288e3a8bf9b4b48
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-08-28 17:29:08 -0400

    Add Sidebar UI for Selecting SDR Function

--- src/components/AppView.tsx
+++ src/components/AppView.tsx
@@ -3,12 +3,62 @@ import RadioView from "@/components/Radio/RadioView";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Station } from "@/lib/types";
 import { getCurrentWebviewWindow } from "@tauri-apps/api/webviewWindow";
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import SdrSelector from "./SdrSelector";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "./ui/resizable";
+import { Button } from "./ui/button";
 
 const appWindow = getCurrentWebviewWindow();
 
+interface ViewData {
+  id: string;
+  name: string;
+  subviews?: ViewData[];
+  view?: any;
+}
+
+const views: ViewData[] = [
+  {
+    id: "radio",
+    name: "Radio",
+    subviews: [
+      {
+        id: "hd-radio",
+        name: "HD Radio",
+        view: RadioView,
+      },
+      {
+        id: "fm-radio",
+        name: "FM Radio",
+        view: RadioView,
+      },
+      {
+        id: "am-radio",
+        name: "AM Radio",
+        view: RadioView,
+      },
+    ],
+  },
+  {
+    id: "aircraft",
+    name: "Aircraft",
+    subviews: [
+      {
+        id: "ads-b",
+        name: "ADS-B",
+        view: AdsbDecoderView,
+      },
+    ],
+  },
+];
+
 export default function AppView() {
+  const [currentViewId, setCurrentViewId] = useState<ReactNode>(views[0].id);
+
   const [isSdrInUse, setIsSdrInUse] = useState(false);
   const [shouldStopAdsb, setShouldStopAdsb] = useState(false);
   const [requestedStation, setRequestedStation] = useState<undefined | Station>(
@@ -48,46 +98,96 @@ export default function AppView() {
   });
 
   return (
-    <main className="flex flex-col h-screen w-screen gap-4">
-      <SdrSelector />
-      <Tabs
-        value={currentTab}
-        onValueChange={async (newTab) => {
-          if (isSdrInUse) {
-            if (currentStation) {
-              setRequestedStation(undefined);
-            } else {
-              setShouldStopAdsb(true);
-              setRequestedTab(newTab);
-            }
-          }
-          setCurrentTab(newTab);
-        }}
-        className="flex flex-col justify-start items-center align-middle h-screen w-screen overflow-hidden"
-      >
-        <TabsList className="mb-2">
-          <TabsTrigger value="radio">Radio</TabsTrigger>
-          <TabsTrigger value="adsb">ADS-B</TabsTrigger>
-        </TabsList>
-        <TabsContent value="radio" className="grow w-full overflow-hidden">
-          <RadioView
-            isSdrInUse={isSdrInUse}
-            setIsSdrInUse={setIsSdrInUse}
-            currentStation={currentStation}
-            setCurrentStation={setCurrentStation}
-            requestedStation={requestedStation}
-            setRequestedStation={setRequestedStation}
-          />
-        </TabsContent>
-        <TabsContent value="adsb" className="grow w-full overflow-hidden">
-          <AdsbDecoderView
-            //isSdrInUse={isSdrInUse}
-            setIsSdrInUse={setIsSdrInUse}
-            shouldStop={shouldStopAdsb}
-            setShouldStop={setShouldStopAdsb}
-          />
-        </TabsContent>
-      </Tabs>
-    </main>
+    <ResizablePanelGroup
+      direction="horizontal"
+      className="flex gap-4 h-screen w-screen"
+    >
+      <ResizablePanel maxSize={20} minSize={15}>
+        <div className="flex flex-col gap-8 p-6">
+          <MapViewData viewData={views} topLevel={true} />
+        </div>
+      </ResizablePanel>
+      <ResizableHandle />
+      <ResizablePanel>
+        <main className="flex flex-col gap-4">
+          <SdrSelector />
+          <Tabs
+            value={currentTab}
+            onValueChange={async (newTab) => {
+              if (isSdrInUse) {
+                if (currentStation) {
+                  setRequestedStation(undefined);
+                } else {
+                  setShouldStopAdsb(true);
+                  setRequestedTab(newTab);
+                }
+              }
+              setCurrentTab(newTab);
+            }}
+            className="flex flex-col justify-start items-center align-middle h-screen w-screen overflow-hidden"
+          >
+            <TabsList className="mb-2">
+              <TabsTrigger value="radio">Radio</TabsTrigger>
+              <TabsTrigger value="adsb">ADS-B</TabsTrigger>
+            </TabsList>
+            <TabsContent value="radio" className="grow w-full overflow-hidden">
+              <RadioView
+                isSdrInUse={isSdrInUse}
+                setIsSdrInUse={setIsSdrInUse}
+                currentStation={currentStation}
+                setCurrentStation={setCurrentStation}
+                requestedStation={requestedStation}
+                setRequestedStation={setRequestedStation}
+              />
+            </TabsContent>
+            <TabsContent value="adsb" className="grow w-full overflow-hidden">
+              <AdsbDecoderView
+                //isSdrInUse={isSdrInUse}
+                setIsSdrInUse={setIsSdrInUse}
+                shouldStop={shouldStopAdsb}
+                setShouldStop={setShouldStopAdsb}
+              />
+            </TabsContent>
+          </Tabs>
+        </main>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
+}
+
+function MapViewData({
+  viewData,
+  topLevel = false,
+}: {
+  viewData: ViewData[];
+  topLevel?: boolean;
+}) {
+  return (
+    <div className={`flex flex-col ${topLevel ? "gap-8" : "gap-0"}`}>
+      {viewData.map((currentViewData) => {
+        return (
+          <div key={currentViewData.name}>
+            {topLevel ? (
+              <>
+                <h2 className="font-bold text-2xl mb-2">
+                  {currentViewData.name}
+                </h2>
+                {currentViewData.subviews && (
+                  <MapViewData viewData={currentViewData.subviews} />
+                )}
+              </>
+            ) : (
+              <Button
+                size="sm"
+                variant="ghost"
+                className="w-full justify-start -mx-[0.625rem]"
+              >
+                {currentViewData.name}
+              </Button>
+            )}
+          </div>
+        );
+      })}
+    </div>
   );
 }
